2ddd1461525e98941f3e09a8e2c2e13310d442d3
Rene <rene_amg@hotmail.com>
2019-02-24 22:26:09 -0600

    basic breadcrum logic, clean sidebar

---
+++ src/app/pages/examples/boxes/boxes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-boxes',
+  templateUrl: './boxes.component.html',
+  styles: []
+})
+export class BoxesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
